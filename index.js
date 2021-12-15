454027bb4069497a5ca662288cbe69a6434ff248
huulamnguyen <liamnguyen.swe@gmail.com>
2021-12-15 09:31:33 -0500

    commit tree 734ce1c81b277c8dd130674a1e379ae81797c3a2

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+// FIRST APPROACH
+// const superbowlWin = (record) => {
+//     const res = record.find( elem => elem.result === "W")
+//     if (res){
+//         return res.year
+//     } else {
+//         return undefined
+//     }
+// }
 
+// SECOND APPROACH
+// function isWin(elem) {
+//     return elem.result === "W"
+// }
+// function superbowlWin(record){
+//     if (record.find(isWin)){
+//         return record.find(isWin).year
+//     } else {
+//         return undefined
+//     }
+// }
+
+// THIRD APPROACH
+const superbowlWin = (record) => {
+    const res = record.find(e => e.result === "W")
+    return res ? res.year : undefined
+}
